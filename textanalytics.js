8597e9899da1b235a08d198c00ac895d5b0b385e
Digvijay Gole <digvijaygole777@outlook.com>
2020-12-16 19:15:46 -0500

    =docs added

--- textanalytics.js
+++ textanalytics.js
@@ -1,9 +1,10 @@
-"use strict";
 const express = require('express')
 const app = express()
 const port = 3000
 const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
-const { text } = require('body-parser');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const cors = require("cors");
 
 const key = '41ca87f466e84a3b9e87428dbd628587';
 const endpoint = 'https://systemint.cognitiveservices.azure.com/';
@@ -11,6 +12,26 @@ const endpoint = 'https://systemint.cognitiveservices.azure.com/';
 const textAnalyticsClient = new TextAnalyticsClient(endpoint,  new AzureKeyCredential(key));
 
 const bodyParser = require('body-parser')
+
+//swagger defination
+const options = {
+    swaggerDefinition: {
+      info: {
+        title: "Text analytics Azure Api",
+        version: "1.0.0",
+        description: "Text,language analysis and keyphrases generator ",
+      },
+      host: "localhost:3000",
+      basePath: "/",
+    },
+    apis: ["./textanalytics.js"],
+  };
+
+const specs = swaggerJsdoc(options);
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+app.use(cors());
 
 app.use(
   bodyParser.urlencoded({
@@ -20,19 +41,49 @@ app.use(
 
 app.use(bodyParser.json())
 
-app.get('/sentiment', async (req, res) => {
  
-        console.log(req.body.sentiment)
+
+ /**
+ * @swagger
+ * /sentiment/{query}:
+ *    get:
+ *      description: Get response
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          200:
+ *              description: Got response 
+ *          500:
+ *              description: Error
+ *      parameters:
+ *          - name: query
+ *            in: path
+ *            required: true
+ *            type: string
+ *
+ */
+app.get('/sentiment/:query', async (req, res) => {
+  
+        var paramater=req.params.query;
+        console.log(paramater)
+
+        var sentimentObj;
+        if(paramater===undefined || paramater===null || paramater===""){
+            res.send('Please send the body')
+        }else{
+
+        
         const sentimentInput = [
-            req.body.sentiment
+            paramater
         ];
 
         try {
 
             const sentimentResult = await textAnalyticsClient.analyzeSentiment(sentimentInput);
+           
             sentimentResult.forEach(document => {
 
-                var sentimentObj={
+                 sentimentObj={
                     id:document.id,
                     document_Sentiment:document.sentiment,
                     scorespositive:document.confidenceScores.positive.toFixed(2),
@@ -41,24 +92,52 @@ app.get('/sentiment', async (req, res) => {
                     sentenceSentiment:document.sentences.length
 
                 }
-               res.send(sentimentObj)
+                
                
             });
-    res.sendStatus(200)
+            res.send(200,sentimentObj)
+            res.end();
 
         } catch (e) {
-           res.sendStatus(500)
+            res.send(e.message); 
         } finally {
             console.log('Sentiment analyzed');
         }
-       
+    }
 })
 
-app.get('/detectlanguage', async (req, res) => {
+
+
+
+ /**
+ * @swagger
+ * /detectlanguage/{query}:
+ *    get:
+ *      description: Get response
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          200:
+ *              description: Got response 
+ *          500:
+ *              description: Error
+ *      parameters:
+ *          - name: query
+ *            in: path
+ *            required: true
+ *            type: string
+ *
+ */
+app.get('/detectlanguage/:query', async (req, res) => {
  
     console.log(req.body.language)
+    var paramater=req.params.query;
+
+    if(paramater===undefined || paramater===null || paramater===""){
+        res.send('Please send the body')
+    }else{
     const languageinput = [
-        req.body.language
+        paramater
     ];
 
     try {
@@ -69,44 +148,70 @@ app.get('/detectlanguage', async (req, res) => {
         res.json('The language used is ' + detectedln);
         
         res.sendStatus(200)
+        res.end();
 
     } catch (e) {
-        res.sendStatus(500) ;
+        res.send(e.message); 
         
     } finally {
         console.log('Language detected');
     }
+}
    
 })
 
 
-app.get('/keyphrases', async (req, res) => {
- 
-     
-    
+/**
+ * @swagger
+ * /keyphrases/{query}:
+ *    get:
+ *      description: Get response
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          200:
+ *              description: Got response 
+ *          500:
+ *              description: Error
+ *      parameters:
+ *          - name: query
+ *            in: path
+ *            required: true
+ *            type: string
+ *
+ */
+app.get('/keyphrases/:query', async (req, res) => {
+
+    var paramater=req.params.query;
+    if(paramater===undefined || paramater===null || paramater===""){
+        res.send('Please send the body')
+    }else{
+  
     const keyphraseInput = [
-        req.body.sentence
+        paramater
     ];
 
     try {
-
         const keyPhraseResult = await textAnalyticsClient.extractKeyPhrases(keyphraseInput);
         console.log(keyPhraseResult)
         keyPhraseResult.forEach(document => {
+
             console.log(document.keyPhrases)
             res.send('Key Phrases : '+document.keyPhrases);
             
         });
         
         res.sendStatus(200)
+        res.end();
 
     } catch (e) {
          
-        res.sendStatus(500) ;
+         res.send(e.message); 
         
     } finally {
         console.log('Key phrases extracted');
     }
+}
    
 })
   
